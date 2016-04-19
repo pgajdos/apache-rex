d52f49bf7a24fe6708003e5c729a890835dccc6a
Petr Gajdos <pgajdos@suse.cz>
2016-04-19 13:51:42 +0200

    use cgi script to show SERVER_ADMIN variable instead of ErrorDocument (more portable for older versions)

--- core-ServerAdmin-basic/run.sh
+++ core-ServerAdmin-basic/run.sh
@@ -1,7 +1,18 @@
 exit_code=0
 
+cgi_dir=$AREX_RUN_DIR/cgi-bin
+mkdir -p  $cgi_dir
+
 # this should return error message with the contact
-echo "[1] ServerAdmin contact inside (custom) error message"
-curl -s http://localhost:$AREX_RUN_PORT/ | grep 'not found, contact i@apache.org if it should exist' || exit_code=1
+echo "[1] ServerAdmin contact accessible trough SERVER_ADMIN internal variable"
+cat << EOF > $cgi_dir/server-admin.cgi
+#!/bin/bash
+echo 'Content-type: text/html'
+echo ''
+echo "server admin: \$SERVER_ADMIN"
+EOF
+chmod 755 $cgi_dir/server-admin.cgi
+curl -s http://localhost:$AREX_RUN_PORT/cgi-bin/server-admin.cgi | grep 'server admin: i@apache.org' || exit_code=1
+
 
 exit $exit_code
