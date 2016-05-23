472f70e58fe52d908873e281282333d41ee18511
Petr Gajdos <pgajdos@suse.cz>
2016-05-23 11:44:14 +0200

    added mod_proxy-long-url-abz53218 example

---
+++ mod_proxy-long-url-abz53218/run.sh
@@ -0,0 +1,9 @@
+exit_code=0
+
+mkdir -p $AREX_RUN_DIR/htdocs-vh/aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb/
+echo 'document' > $AREX_RUN_DIR/htdocs-vh/aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb/document.html
+
+curl -s http://localhost:$AREX_PORT/vh/document.html | grep document || exit_code=1
+
+exit $exit_code
+
