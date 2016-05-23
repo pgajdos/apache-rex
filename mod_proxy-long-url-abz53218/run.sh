71143ff2df5ee8f0a6d7506d68edd37ddabea9be
Petr Gajdos <pgajdos@suse.cz>
2016-05-23 14:12:29 +0200

    add subtest description

--- mod_proxy-long-url-abz53218/run.sh
+++ mod_proxy-long-url-abz53218/run.sh
@@ -3,6 +3,7 @@ exit_code=0
 mkdir -p $AREX_RUN_DIR/htdocs-vh/aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb/
 echo 'document' > $AREX_RUN_DIR/htdocs-vh/aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb/document.html
 
+echo "[1] get the document trough proxy on long url"
 curl -s http://localhost:$AREX_PORT/vh/document.html | grep document || exit_code=1
 
 exit $exit_code
