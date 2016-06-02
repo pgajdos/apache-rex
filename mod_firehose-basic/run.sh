9c7416ab12181d6d11d2b948edee1eb3f7854eb0
Petr Gajdos <pgajdos@suse.cz>
2016-06-02 13:03:25 +0200

    add comments

--- mod_firehose-basic/run.sh
+++ mod_firehose-basic/run.sh
@@ -6,6 +6,7 @@ echo 'document' > $AREX_DOCUMENT_ROOT/document.html
 curl http://localhost:$AREX_PORT/
 curl http://localhost:$AREX_PORT/document.html
 
+# just informative output
 echo '.................................................'
 echo 'request-input.firehose'
 echo 'length : timestamp : req(<)/res(>) : uuid : count'
@@ -19,6 +20,7 @@ echo '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 cat $AREX_RUN_DIR/request-output.firehose
 echo '.................................................'
 
+# test itself
 echo
 echo "[1] input log contains dump of requests"
 grep 'GET /document.html HTTP/1.1' $AREX_RUN_DIR/request-input.firehose  || exit_code=1
