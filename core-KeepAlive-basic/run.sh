7df433ebf2025458b0e0af0b04264b9b1baef12f
Petr Gajdos <pgajdos@suse.cz>
2016-06-06 17:08:28 +0200

    make test work with older curl

--- core-KeepAlive-basic/run.sh
+++ core-KeepAlive-basic/run.sh
@@ -11,11 +11,11 @@ cp apache_pb.png      $AREX_RUN_DIR/htdocs-vh2/
 echo "[1] connection was reused when KeepAlive on"
 curl -s -v http://localhost:$AREX_PORT1/ \
            http://localhost:$AREX_PORT1/apache_pb.png > $AREX_RUN_DIR/out-vh1 2>&1
-grep 'Re-using existing connection!' $AREX_RUN_DIR/out-vh1 || exit_code=1
+grep -a 'Re-using existing connection!' $AREX_RUN_DIR/out-vh1 || exit_code=1
 
 echo "[2] there was two connections when KeepAlive off"
 curl -s -v http://localhost:$AREX_PORT2/ \
            http://localhost:$AREX_PORT2/apache_pb.png > $AREX_RUN_DIR/out-vh2 2>&1
-grep 'Closing connection 1'          $AREX_RUN_DIR/out-vh2 || exit_code=2
+grep -a 'Re-using existing connection'  $AREX_RUN_DIR/out-vh2 && exit_code=2
 
 exit $exit_code
