4fecb90c26078c76b1a6181a6bbe37c40371c73e
Petr Gajdos <pgajdos@suse.cz>
2016-08-26 09:04:55 +0200

    evaluate result of the test

--- devel-mod_example_1/run.sh
+++ devel-mod_example_1/run.sh
@@ -3,9 +3,9 @@ exit_code=0
 echo 'index page' > $AREX_DOCUMENT_ROOT/index.html
 
 echo "[1] example-handler is invoked"
-curl http://localhost:$AREX_PORT/hello
+curl -s http://localhost:$AREX_PORT/hello | grep 'Hello, world!' || exit_code=1
 
 echo "[2] example-handler is NOT invoked"
-curl http://localhost:$AREX_PORT/
+curl -s http://localhost:$AREX_PORT/      | grep 'index page'    || exit_code=2
 
 exit $exit_code
