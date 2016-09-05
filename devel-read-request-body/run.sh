5a6e200f2f8d456ae70376a5a2f4d0b517d2ee7c
Petr Gajdos <pgajdos@suse.cz>
2016-09-05 18:34:17 +0200

    evaluate the exmple

--- devel-read-request-body/run.sh
+++ devel-read-request-body/run.sh
@@ -1,6 +1,10 @@
 exit_code=0
 
 echo "[1] echo request body"
-curl -s -X POST -d 'This is content of a request body.' http://localhost:$AREX_PORT/body-echo
+curl -s -X POST -d 'This is content of a request body.' http://localhost:$AREX_PORT/body-echo \
+ > $AREX_RUN_DIR/out
+
+grep 'Read: 34 bytes'                     $AREX_RUN_DIR/out || exit_code=1
+grep 'This is content of a request body.' $AREX_RUN_DIR/out || exit_code=1
 
 exit $exit_code
