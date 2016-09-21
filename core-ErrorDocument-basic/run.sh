a0033bff597debc25100f061981b96a9d46c74aa
Petr Gajdos <pgajdos@suse.cz>
2016-09-21 11:16:03 +0200

    extend core-ErrorDocument-basic to test default error document

--- core-ErrorDocument-basic/run.sh
+++ core-ErrorDocument-basic/run.sh
@@ -10,5 +10,8 @@ curl -s http://localhost:$AREX_PORT/ | grep 'the page not found' || exit_code=1
 echo "[2] ErrorDocument with document file argument"
 curl -s http://localhost:$AREX_PORT/private/ | grep '<h1>forbidden for you</h1>' || exit_code=2
 
+echo "[3] ErrorDocument is not defined for this error, default taken"
+curl -X NOTIMPLEMENTEDMETHOD -s http://localhost:$AREX_PORT/ | grep '501 Not Implemented\|NOTIMPLEMENTEDMETHOD' || exit_code=3
+
 exit $exit_code
 
