4e7b2a561b3314dc21d1209cb73c163fd7f3db68
Petr Gajdos <pgajdos@suse.cz>
2016-09-22 13:14:37 +0200

    fix exit values, improve third example

--- mod_filter-AddOutputFilterByType/run.sh
+++ mod_filter-AddOutputFilterByType/run.sh
@@ -8,11 +8,11 @@ curl -s http://localhost:$AREX_PORT/text-xml/test.xml | grep 'HELLOWORLD' || exi
 echo "[2] document test.xml (content: helloworld) is NOT recognized as application/xml"
 mkdir -p $AREX_DOCUMENT_ROOT/application-xml
 echo 'helloworld' > $AREX_DOCUMENT_ROOT/application-xml/test.xml
-curl -s http://localhost:$AREX_PORT/application-xml/test.xml | grep  'helloworld' || exit_code=1
+curl -s http://localhost:$AREX_PORT/application-xml/test.xml | grep  'helloworld' || exit_code=2
 
-echo "[3] document test.xml (content: helloworld) is recognized as application/xml when AddType used"
-mkdir -p $AREX_DOCUMENT_ROOT/application-xml-defined
-echo 'helloworld' > $AREX_DOCUMENT_ROOT/application-xml-defined/test.xml
-curl -s http://localhost:$AREX_PORT/application-xml-defined/test.xml # | grep  'helloworld' || exit_code=1
+echo "[3] document test.xml (content: helloworld) is recognized as foo/xml when appropriate AddType used"
+mkdir -p $AREX_DOCUMENT_ROOT/foo-xml
+echo 'helloworld' > $AREX_DOCUMENT_ROOT/foo-xml/test.xml
+curl -s http://localhost:$AREX_PORT/foo-xml/test.xml  | grep  'HELLOWORLD' || exit_code=3
 
 exit $exit_code
