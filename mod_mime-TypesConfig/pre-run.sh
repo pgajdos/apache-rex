462361f01fdb92340caf3b6077878e266a064943
Petr Gajdos <pgajdos@suse.cz>
2016-11-03 13:11:53 +0100

    add two subexamples on mime type <-> extension map priority

--- mod_mime-TypesConfig/pre-run.sh
+++ mod_mime-TypesConfig/pre-run.sh
@@ -1,1 +1,12 @@
-echo 'bar/tst tst' > $AREX_RUN_DIR/mime.types
+cat << EOF > $AREX_RUN_DIR/mime.types
+bar/tst         tst
+# application/xml is shadowed by text/xml for xml
+application/xml xml
+text/xml        xml
+# text/xml is shadowed by application/xml for xml2
+text/xml        xml2
+application/xml xml2
+# my-text/txt will be shadowed by your-text/doc (by AddType)
+my-text/txt     txt
+EOF
+
--- mod_mime-TypesConfig/run.sh
+++ mod_mime-TypesConfig/run.sh
@@ -2,6 +2,19 @@ exit_code=0
 
 echo "[1] document test.tst is recognized as bar/tst"
 echo 'helloworld' > $AREX_DOCUMENT_ROOT/test.tst
-curl -s http://localhost:$AREX_PORT/test.tst  | grep  'HELLOWORLD' || exit_code=4
+curl -s http://localhost:$AREX_PORT/test.tst  | grep  'HELLOWORLD' || exit_code=1
+
+# precence in mime.types
+# inspired by resolution of http://marc.info/?l=apache-httpd-users&m=147444867019367
+echo "[2] former occurence of the same extension is shadowed by latter one"
+echo 'hiho'  > $AREX_DOCUMENT_ROOT/test.xml
+curl -s http://localhost:$AREX_PORT/test.xml  | grep 'hiho' || exit_code=2
+echo 'blebleble' > $AREX_DOCUMENT_ROOT/test.xml2
+curl -s http://localhost:$AREX_PORT/test.xml2 | grep 'BLEBLEBLE' || exit_code=2
+
+# precedence off AddType before mime.types
+echo "[3] AddType shadows assignment in mime.types for the same extension"
+echo 'tchuss' > $AREX_DOCUMENT_ROOT/test.txt
+curl -s http://localhost:$AREX_PORT/test.txt | grep 'tchuss' || exit_code=3 
 
 exit $exit_code
