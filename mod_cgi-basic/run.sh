76368700d58166f37e36fafcd7e7cb25f4600ade
Petr Gajdos <pgajdos@suse.cz>
2017-01-13 18:46:12 +0100

    add perl cgi script example to mod_cgi-basic

--- mod_cgi-basic/run.sh
+++ mod_cgi-basic/run.sh
@@ -25,5 +25,19 @@ echo "[3] correct cgi script without executable bits"
 chmod 644 $cgi_dir/correct.cgi
 curl -s http://localhost:$AREX_PORT/cgi-bin/correct.cgi | grep '500 Internal Server Error' || exit_code=3
 
+echo "[4] perl cgi script"
+cat << EOF > $cgi_dir/script.pl
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+print "Content-type: text/html\r\n";
+print "\r\n";
+print "perl cgi example\r\n";
+1;
+EOF
+chmod 755 $cgi_dir/script.pl
+curl -s http://localhost:$AREX_PORT/cgi-bin/script.pl | grep 'perl cgi example' || exit_code=4
+
 exit $exit_code
 
