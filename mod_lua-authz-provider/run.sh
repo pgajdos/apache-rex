39da69e8deacd70ca8a714817b707226f32e4a34
Petr Gajdos <pgajdos@suse.cz>
2017-02-17 10:54:31 +0100

    fix syntax of ipv6 localhost address

--- mod_lua-authz-provider/run.sh
+++ mod_lua-authz-provider/run.sh
@@ -7,7 +7,7 @@ echo "[1] access denied when user agent address is not ::1 and user was not auth
 curl -s http://127.0.0.1:$AREX_PORT/ | grep '401 Unauthorized' || exit_code=1
 
 echo "[2] access allowed for ::1 user agent address"
-curl -s http://::1:$AREX_PORT/ | grep 'private area index' || exit_code=2
+curl -s http://[::1]:$AREX_PORT/ | grep 'private area index' || exit_code=2
 
 echo "[3] access allowed for authenticated user"
 curl -s -u john_doe:StrongPassword http://127.0.0.1:$AREX_PORT/ | grep 'private area index' || exit_code=3
