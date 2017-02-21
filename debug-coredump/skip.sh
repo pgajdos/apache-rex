0424ac9dd4c85c8c17c885b7973b0629731a22d1
Petr Gajdos <pgajdos@suse.cz>
2017-02-21 11:15:34 +0100

    mising word

--- debug-coredump/skip.sh
+++ debug-coredump/skip.sh
@@ -3,7 +3,7 @@ skip_exit_code=1
 [ "$(ulimit -c)" == "0" ] && skip_exit_code=0
 # System appears to write coredumps to systemd-coredump
 # but do not have coredumpctl. I have no solution for 
-# situation now.
+# this situation now.
 [ "$AREX_HAVE_SYSTEMD_COREDUMP" == "1" ] && ! [ $(which coredumpctl 2>/dev/null) ] && skip_exit_code=0
 exit $skip_exit_code
 
