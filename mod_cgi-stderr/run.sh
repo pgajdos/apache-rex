8ceacf64d005fd3783c45394a028eabafcabb760
Petr Gajdos <pgajdos@suse.cz>
2017-05-12 09:04:46 +0200

    add mod_cgi-stderr

---
+++ mod_cgi-stderr/run.sh
@@ -0,0 +1,19 @@
+exit_code=0
+
+cgi_dir=$AREX_RUN_DIR/cgi-bin
+mkdir -p  $cgi_dir
+
+echo "[1] script output to stderr is seen in error_log"
+cat << EOF > $cgi_dir/stderr.cgi
+#!/bin/bash
+echo 'Content-type: text/html'
+echo ''
+echo 'Sending a message to stderr'
+echo 'I am an ERROR' 1>&2
+EOF
+chmod 755 $cgi_dir/stderr.cgi
+curl -s http://localhost:$AREX_PORT/cgi-bin/stderr.cgi
+grep 'I am an ERROR' $AREX_RUN_DIR/error_log || exit_code=1
+
+exit $exit_code
+
