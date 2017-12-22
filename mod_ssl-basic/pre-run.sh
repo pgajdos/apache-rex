66f12a29ad67b56b9cf5170b8e1f16334c852ca8
Petr Gajdos <pgajdos@suse.cz>
2017-12-22 11:28:47 +0100

    openssl functions for creating certificates, use them in mod_ssl-basic

--- mod_ssl-basic/pre-run.sh
+++ mod_ssl-basic/pre-run.sh
@@ -1,3 +1,30 @@
-# this need to be done before apache start
-cp test-server.key test-server.crt $AREX_RUN_DIR
+#
+# create CA certificate and SERVER certificate
+#
+. ../lib/openssl
+mkdir -p $AREX_RUN_DIR/cert
+server_name='test'
+echo Generating CA certificate
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~
+# create CA key
+openssl_key $AREX_RUN_DIR/cert/test-ca.key
+# create CA certificate using CA key
+openssl_ca_cert $AREX_RUN_DIR/cert/test-ca.key $AREX_RUN_DIR/cert/test-ca.crt 'trustworthy-ca'
+echo
+echo Generating SERVER certificate
+echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# create SERVER key
+openssl_key $AREX_RUN_DIR/cert/test-server.key
+# create SERVER signing request using SERVER key
+openssl_server_csr $AREX_RUN_DIR/cert/test-server.key $AREX_RUN_DIR/cert/test-server.csr $server_name
+# create SERVER certificate using CA key and certificate and SERVER signing request
+openssl_server_cert $AREX_RUN_DIR/cert/test-ca.crt \
+                    $AREX_RUN_DIR/cert/test-ca.key \
+                    $AREX_RUN_DIR/cert/test-server.csr \
+                    $AREX_RUN_DIR/cert/test-server.crt \
+                    $server_name
+
+#
+# create runtime dir
+#
 mkdir -p $AREX_RUN_DIR/run
--- mod_ssl-basic/run.sh
+++ mod_ssl-basic/run.sh
@@ -3,8 +3,8 @@ exit_code=0
 echo 'Test SSL' > $AREX_DOCUMENT_ROOT/index.html
 
 echo "[1] access trough https allowed"
-curl -s --cacert test-ca.crt --resolve "test:$AREX_PORT:127.0.0.1" https://test:$AREX_PORT/ | grep 'Test SSL' || exit_code=1
+curl -s --cacert $AREX_RUN_DIR/cert/test-ca.crt --resolve "test:$AREX_PORT:127.0.0.1" https://test:$AREX_PORT/ | grep 'Test SSL' || exit_code=1
 echo "[2] access trough http disallowed"
-curl -s --cacert test-ca.crt --resolve "test:$AREX_PORT:127.0.0.1" http://test:$AREX_PORT/  | grep '400 Bad Request' || exit_code=2
+curl -s --cacert $AREX_RUN_DIR/cert/test-ca.crt --resolve "test:$AREX_PORT:127.0.0.1" http://test:$AREX_PORT/  | grep '400 Bad Request' || exit_code=2
 
 exit $exit_code
