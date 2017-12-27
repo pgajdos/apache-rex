dfb7077a3f022c79ad3148328f16f4af67379730
Petr Gajdos <pgajdos@suse.cz>
2017-12-27 16:12:46 +0100

    lbmethod_traffic requires more trafic, otherwise sometimes (e. g. on i586 obs worker) it happend that backend1 get all requests

--- mod_macro-basic/run.sh
+++ mod_macro-basic/run.sh
@@ -2,12 +2,12 @@ exit_code=0
 
 for id in backend1 backend2 fallback; do
   mkdir -p $AREX_RUN_DIR/htdocs-vh-$id
-  echo "server $id" > $AREX_RUN_DIR/htdocs-vh-$id/index.html
+  printf "server $id\n$(seq 1 1000000)" > $AREX_RUN_DIR/htdocs-vh-$id/index.html
 done
 
 echo "[1] VirtualHosts are properly defined"
 for i in $(seq 1 20); do 
-  curl -s http://localhost:60084/
+  curl -s http://localhost:60084/ | head -n 1
 done | tee $AREX_RUN_DIR/run.out
 grep 'server backend2' $AREX_RUN_DIR/run.out || exit_code=1
 grep 'server backend1' $AREX_RUN_DIR/run.out || exit_code=1
