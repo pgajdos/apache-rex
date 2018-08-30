541acf69fcc2433efe8268a9614a03974d0251e0
pgajdos <pgajdos@suse.cz>
2018-08-30 11:59:14 +0200

    core-Satisfy-basic works in 2.4

--- core-Satisfy-basic/run.sh
+++ core-Satisfy-basic/run.sh
@@ -10,6 +10,6 @@ curl -s http://localhost:$AREX_PORT/any/ | grep 'content allowed for localhost O
 echo "[2] allowed because all of conditions are met: localhost, valid-user"
 curl -s -u john:StrongPassword http://localhost:$AREX_PORT/all/ | grep 'content allowed for localhost AND valid-user' || exit_code=2
 echo "[3] denied because valid-user is not met"
-curl -s http://localhost:$AREX_PORT/all/ | grep '401 Authorization Required' || exit_code=3
+curl -s http://localhost:$AREX_PORT/all/ | grep '401 .*uthoriz' || exit_code=3
 
 exit $exit_code
