5517610dfcf6cbab9a65c6f7b0fbe99a4cd0494d
pgajdos <pgajdos@suse.cz>
2018-11-02 10:52:25 +0100

    amend mod_ssl-verify-client to reflect openssl refactoring

--- mod_ssl-verify-client/pre-run.sh
+++ mod_ssl-verify-client/pre-run.sh
@@ -3,41 +3,18 @@
 # server and client share the same CA
 #
 . ../lib/openssl
-mkdir -p $AREX_RUN_DIR/cert
-server_name='test'
-client_name='joe'
-echo Generating CA certificate
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~
-# create CA key
-openssl_key $AREX_RUN_DIR/cert/test-ca.key
-# create CA certificate using CA key
-openssl_ca_cert $AREX_RUN_DIR/cert/test-ca.key $AREX_RUN_DIR/cert/test-ca.crt 'trustworthy-ca'
+echo Setup CA
+echo ~~~~~~~~
+openssl_setup_ca $AREX_RUN_DIR
 echo
-echo Generating SERVER certificate
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# create SERVER key
-openssl_key $AREX_RUN_DIR/cert/test-server.key
-# create SERVER signing request using SERVER key
-openssl_server_csr $AREX_RUN_DIR/cert/test-server.key $AREX_RUN_DIR/cert/test-server.csr $server_name
-# create SERVER certificate using CA key and certificate and SERVER signing request
-openssl_server_cert $AREX_RUN_DIR/cert/test-ca.crt \
-                    $AREX_RUN_DIR/cert/test-ca.key \
-                    $AREX_RUN_DIR/cert/test-server.csr \
-                    $AREX_RUN_DIR/cert/test-server.crt \
-                    $server_name
+echo Setup SERVER 
+echo ~~~~~~~~~~~~
+openssl_setup_entity $AREX_RUN_DIR aserver.suse.cz
 echo
-echo Generating CLIENT certificate
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# create CLIENT key
-openssl_key $AREX_RUN_DIR/cert/test-client.key
-# create CLIENT signing request using CLIENT key
-openssl_client_csr $AREX_RUN_DIR/cert/test-client.key $AREX_RUN_DIR/cert/test-client.csr $client_name
-# create CLIENT certificate using CA key and certificate and CLIENT signing request
-openssl_client_cert $AREX_RUN_DIR/cert/test-ca.crt \
-                    $AREX_RUN_DIR/cert/test-ca.key \
-                    $AREX_RUN_DIR/cert/test-client.csr \
-                    $AREX_RUN_DIR/cert/test-client.crt \
-                    $client_name
+echo Setup CLIENT
+echo ~~~~~~~~~~~~
+openssl_setup_entity $AREX_RUN_DIR aclient.suse.cz
+echo
 
 #
 # create runtime dir, see DefaultRuntimeDir directive
--- mod_ssl-verify-client/run.sh
+++ mod_ssl-verify-client/run.sh
@@ -3,14 +3,14 @@ exit_code=0
 echo 'Test SSL, restricted area' > $AREX_DOCUMENT_ROOT/index.html
 
 echo "[1] access for not authentificated client disallowed"
-curl --cacert $AREX_RUN_DIR/cert/test-ca.crt --resolve "test:$AREX_PORT:127.0.0.1" https://test:$AREX_PORT/ 2>&1 \
+curl --cacert $AREX_RUN_DIR/ca/my.crt --resolve "aserver.suse.cz:$AREX_PORT:127.0.0.1" https://aserver.suse.cz:$AREX_PORT/ 2>&1 \
   | grep 'handshake failure' || exit_code=1
 
 echo "[2] client correctly verified"
-curl -s --cacert $AREX_RUN_DIR/cert/test-ca.crt \
-        --cert   $AREX_RUN_DIR/cert/test-client.crt \
-        --key    $AREX_RUN_DIR/cert/test-client.key  \
-        --resolve "test:$AREX_PORT:127.0.0.1" https://test:$AREX_PORT/ \
+curl -s --cacert $AREX_RUN_DIR/ca/my.crt \
+        --cert   $AREX_RUN_DIR/aclient.suse.cz/my.crt \
+        --key    $AREX_RUN_DIR/aclient.suse.cz/private.key  \
+        --resolve "aserver.suse.cz:$AREX_PORT:127.0.0.1" https://aserver.suse.cz:$AREX_PORT/ \
   | grep 'Test SSL' || exit_code=2
  
 exit $exit_code
