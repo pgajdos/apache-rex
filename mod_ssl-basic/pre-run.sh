841a2cefafad602f5cbf1ecb403858002895378b
pgajdos <pgajdos@suse.cz>
2018-11-02 10:49:03 +0100

    amend mod_ssl-basic to reflect openssl refactoring

--- mod_ssl-basic/pre-run.sh
+++ mod_ssl-basic/pre-run.sh
@@ -2,28 +2,14 @@
 # create CA certificate and SERVER certificate
 #
 . ../lib/openssl
-mkdir -p $AREX_RUN_DIR/cert
-server_name='test'
-echo Generating CA certificate
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~
-# create CA key
-openssl_key $AREX_RUN_DIR/cert/test-ca.key
-# create CA certificate using CA key
-openssl_ca_cert $AREX_RUN_DIR/cert/test-ca.key $AREX_RUN_DIR/cert/test-ca.crt 'trustworthy-ca'
+echo Setup CA
+echo ~~~~~~~~
+openssl_setup_ca $AREX_RUN_DIR
 echo
-echo Generating SERVER certificate
-echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# create SERVER key
-openssl_key $AREX_RUN_DIR/cert/test-server.key
-# create SERVER signing request using SERVER key
-openssl_server_csr $AREX_RUN_DIR/cert/test-server.key $AREX_RUN_DIR/cert/test-server.csr $server_name
-# create SERVER certificate using CA key and certificate and SERVER signing request
-openssl_server_cert $AREX_RUN_DIR/cert/test-ca.crt \
-                    $AREX_RUN_DIR/cert/test-ca.key \
-                    $AREX_RUN_DIR/cert/test-server.csr \
-                    $AREX_RUN_DIR/cert/test-server.crt \
-                    $server_name
-
+echo Setup SERVER 
+echo ~~~~~~~~~~~~
+openssl_setup_entity $AREX_RUN_DIR aserver.suse.cz
+echo
 #
 # create runtime dir, see DefaultRuntimeDir directive
 #
--- mod_ssl-basic/run.sh
+++ mod_ssl-basic/run.sh
@@ -3,8 +3,12 @@ exit_code=0
 echo 'Test SSL' > $AREX_DOCUMENT_ROOT/index.html
 
 echo "[1] access trough https allowed"
-curl -s --cacert $AREX_RUN_DIR/cert/test-ca.crt --resolve "test:$AREX_PORT:127.0.0.1" https://test:$AREX_PORT/ | grep 'Test SSL' || exit_code=1
+curl -s --cacert $AREX_RUN_DIR/ca/my.crt --resolve "aserver.suse.cz:$AREX_PORT:127.0.0.1" https://aserver.suse.cz:$AREX_PORT/ \
+    | grep 'Test SSL' || exit_code=1
 echo "[2] access trough http disallowed"
-curl -s --cacert $AREX_RUN_DIR/cert/test-ca.crt --resolve "test:$AREX_PORT:127.0.0.1" http://test:$AREX_PORT/  | grep '400 Bad Request' || exit_code=2
-
+curl -s --cacert $AREX_RUN_DIR/ca/my.crt --resolve "aserver.suse.cz:$AREX_PORT:127.0.0.1" http://aserver.suse.cz:$AREX_PORT/ \
+    | grep '400 Bad Request' || exit_code=2
+echo "[3] access to the same address under different name not allowed"
+curl -v -s --cacert $AREX_RUN_DIR/ca/my.crt --resolve "test.suse.cz:$AREX_PORT:127.0.0.1" https://test.suse.cz:$AREX_PORT/ \
+    2>&1 | grep 'does not match target host name' || exit_code=3
 exit $exit_code
