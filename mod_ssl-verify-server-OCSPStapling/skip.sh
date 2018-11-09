1e180a08d7f80d85e0a20bc4467010e6a6cd12e9
pgajdos <pgajdos@suse.cz>
2018-11-09 16:20:09 +0100

    check whether curl support --check-status and do not run mod_ssl-verify-server-OCSPStapling if not

--- mod_ssl-verify-server-OCSPStapling/skip.sh
+++ mod_ssl-verify-server-OCSPStapling/skip.sh
@@ -1,2 +1,2 @@
-# REASON: curl does not have --resolve
-exit $([ $AREX_CURL_HAVE_RESOLVE -eq 0 ])
+# REASON: curl does not have --resolve or does not have --cert-status
+exit $([ $AREX_CURL_HAVE_RESOLVE -eq 0 -o $AREX_CURL_HAVE_CERT_STATUS -eq 0 ])
