a369d68d8f4049d423a837f644d1301e024f9cb6
pgajdos <pgajdos@suse.cz>
2019-01-22 20:34:10 +0100

    add subexample description

--- mod_ssl-SSLCryptoDevice/run.sh
+++ mod_ssl-SSLCryptoDevice/run.sh
@@ -8,6 +8,7 @@ curl -s --cacert $AREX_RUN_DIR/ca/my.crt --resolve "aserver.suse.cz:$AREX_PORT:1
 echo "[2] access trough http disallowed"
 curl -s --cacert $AREX_RUN_DIR/ca/my.crt --resolve "aserver.suse.cz:$AREX_PORT:127.0.0.1" http://aserver.suse.cz:$AREX_PORT/ \
     | grep '400 Bad Request' || exit_code=2
+echo "[3] access to the same address under different name not allowed"
 curl -v -s --cacert $AREX_RUN_DIR/ca/my.crt --resolve "test.suse.cz:$AREX_PORT:127.0.0.1" https://test.suse.cz:$AREX_PORT/ \
     2>&1 | grep 'does not match target host name' || exit_code=3
 exit $exit_code
