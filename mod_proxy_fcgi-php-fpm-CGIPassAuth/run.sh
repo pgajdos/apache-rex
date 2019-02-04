6cfb542c1df64b4e223954d06002dbcf43ee541d
pgajdos <pgajdos@suse.cz>
2019-02-04 18:24:24 +0100

    actually check the output in the example

--- mod_proxy_fcgi-php-fpm-CGIPassAuth/run.sh
+++ mod_proxy_fcgi-php-fpm-CGIPassAuth/run.sh
@@ -2,13 +2,13 @@
 exit_code=0
 
 htpasswd -bc $AREX_RUN_DIR/htpasswd john StrongPassword
-echo '<?php print "Hello $_SERVER['REMOTE_USER']! Your password has leaked: $_SERVER['PHP_AUTH_PW']\n";  ?>' \
+echo '<?php print "Hey $_SERVER['REMOTE_USER']! Your password has leaked: $_SERVER['PHP_AUTH_PW']\n";  ?>' \
   > $AREX_DOCUMENT_ROOT/welcome.php
 
 start_fpm
 
 echo "[1] REMOTE_USER is passed to php script"
-curl -s -u john:StrongPassword http://localhost:$AREX_PORT/welcome.php
+curl -s -u john:StrongPassword http://localhost:$AREX_PORT/welcome.php | grep 'Hey john' || exit_code=1
 
 echo
 stop_fpm
