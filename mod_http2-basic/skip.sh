72cd921aa3374eb5960aa072f539d4eaac836061
pgajdos <pgajdos@suse.cz>
2019-02-20 18:15:36 +0100

    add note

--- mod_http2-basic/skip.sh
+++ mod_http2-basic/skip.sh
@@ -2,5 +2,7 @@
 # from error_log: ' The mpm module (prefork.c) is not supported by mod_http2. The
 # mpm determines  how things are processed in your server. HTTP/2 has more demands
 # in this regard  and the currently selected mpm will just not do.
+#
+# note: see mod_http2-push how to use nghttp2 instead of curl
 exit $([ $AREX_OPENSSL_HAVE_ALPN -eq 0 ] || [ $AREX_MPM == "prefork" ] || [ $AREX_CURL_HAVE_HTTP2 -eq 0 ])
 
