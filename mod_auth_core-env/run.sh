6fec692221f0b1beabcd4514d944c5582186a95d
pgajdos <pgajdos@suse.cz>
2019-06-07 09:48:14 +0200

    actually check for return codes

--- mod_auth_core-env/run.sh
+++ mod_auth_core-env/run.sh
@@ -2,10 +2,10 @@ exit_code=0
 echo 'It works!' > $AREX_DOCUMENT_ROOT/index.html
 
 echo "[1] allowed access trough curl command"
-curl -s http://localhost:$AREX_PORT/ | grep 'It works!'
+curl -v -s http://localhost:$AREX_PORT/ 2>&1 | grep 'User-Agent\|It works!' || exit_code=1
 
 echo "[2] access forbiddend for wget command"
-wget http://localhost:$AREX_PORT/ 2>&1 | grep '403: Forbidden' 
+wget http://localhost:$AREX_PORT/ 2>&1 | grep '403: Forbidden' || exit_code=2
 
 exit $exit_code
 
