b98f5b58b1ae61444f1e9dbada0a451609486fee
pgajdos <pgajdos@suse.cz>
2019-06-28 13:13:51 +0200

    actually check the output

--- mod_nss-basic/run.sh
+++ mod_nss-basic/run.sh
@@ -3,6 +3,6 @@ exit_code=0
 echo 'HTTPS HELLO' > $AREX_DOCUMENT_ROOT/index.html
 
 echo "[1] access trough https"
-curl -k https://localhost:$AREX_PORT/
+curl -s -k https://localhost:$AREX_PORT/ | grep 'HTTPS HELLO' || exit_code=1
 
 exit $exit_code
