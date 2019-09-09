d7307482ea66a41acf7869649dff6964164d8ebf
pgajdos <pgajdos@suse.cz>
2019-09-09 21:14:58 +0200

    add logging possibility

--- core-KeepAlive-basic/run.sh
+++ core-KeepAlive-basic/run.sh
@@ -18,4 +18,11 @@ curl -s -v http://localhost:$AREX_PORT2/ \
            http://localhost:$AREX_PORT2/apache_pb.png > $AREX_RUN_DIR/out-vh2 2>&1
 grep -a 'Re-using existing connection'  $AREX_RUN_DIR/out-vh2 && exit_code=2
 
+if [ $AREX_APACHE_VERSION -ge 20400 ]; then
+  echo "[3] error_log containing requests to connections according"
+  cat $AREX_RUN_DIR/error_log | grep 'Request [01]' | tee $AREX_RUN_DIR/requests-to-conections
+  [ $(grep -c 'Request 0' $AREX_RUN_DIR/requests-to-conections) -eq 3 ] || exit_code=3
+  [ $(grep -c 'Request 1' $AREX_RUN_DIR/requests-to-conections) -eq 1 ] || exit_code=3
+fi
+
 exit $exit_code
