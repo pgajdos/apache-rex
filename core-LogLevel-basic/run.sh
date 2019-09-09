ebd4978ba88a5478557001a83bee5410b63cff2c
pgajdos <pgajdos@suse.cz>
2019-09-09 20:16:34 +0200

    expand example to depend on Location, too

--- core-LogLevel-basic/run.sh
+++ core-LogLevel-basic/run.sh
@@ -1,9 +1,18 @@
 exit_code=0
 
-echo "[1] LogLevel setting depending on %{REMOTE_ADDR}"
-curl -s http://localhost:$AREX_PORT/ > /dev/null
+mkdir $AREX_DOCUMENT_ROOT/no-problem/
+echo 'index' > $AREX_DOCUMENT_ROOT/no-problem/index.html
+
+echo "[1] LogLevel setting depending on %{REMOTE_ADDR} and location"
+curl -s http://localhost:$AREX_PORT/problem/ > /dev/null
+curl -s http://localhost:$AREX_PORT/no-problem/ > /dev/null
 if [ $AREX_APACHE_VERSION -ge 20400 ]; then
-  grep 'http:trace[1-8]' $AREX_RUN_DIR/error_log || exit_code=1
+  echo 'trace3 in both requests'
+  grep 'http:trace3.*Response' $AREX_RUN_DIR/error_log | tee $AREX_RUN_DIR/trace3-response.txt
+  [ $(cat $AREX_RUN_DIR/trace3-response.txt | wc -l) -eq 2 ] || exit_code=1
+  echo 'trace4 in first only'
+  grep 'http:trace4.*Content-Length' $AREX_RUN_DIR/error_log | tee $AREX_RUN_DIR/trace4-content-length.txt
+  [ $(cat $AREX_RUN_DIR/trace4-content-length.txt | wc -l) -eq 1 ] || exit_code=1
 else
   grep '\[debug\]' $AREX_RUN_DIR/error_log || exit_code=1
 fi
