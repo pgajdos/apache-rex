4c1b91be560b49583a6dd90d72d11f4495c10f14
pgajdos <pgajdos@suse.cz>
2019-09-09 13:24:25 +0200

    added subexample [2]

--- core-ErrorLogFormat-basic/run.sh
+++ core-ErrorLogFormat-basic/run.sh
@@ -3,4 +3,11 @@ exit_code=0
 echo "[1] error_log contains specified format"
 cat $AREX_RUN_DIR/error_log | grep "[MYLOGFORMAT].*[MYLOGFORMAT]" || exit_code=1
 
+echo "[2] server time (time in an error_log) timezone matches system one"
+servertimesec=$(tail -n 1 $AREX_RUN_DIR/error_log | sed 's:.*[DATE]\(.*\)[DATE].*::' | date +%s)
+systemtimesec=$(date +%s)
+difference=$((systemtimesec-servertimesec))
+echo $servertimesec - $systemtimesec = $difference
+[ $difference -lt 5 ] || exit_code=2
+
 exit $exit_code
