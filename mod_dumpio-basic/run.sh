320a2abe96ba83c3cc62c33999162f655087eb96
pgajdos <pgajdos@suse.cz>
2019-09-13 18:31:44 +0200

    expand example

--- mod_dumpio-basic/run.sh
+++ mod_dumpio-basic/run.sh
@@ -1,11 +1,15 @@
 exit_code=0
 
-echo index >  $AREX_DOCUMENT_ROOT/index.html
+echo 'my index' >  $AREX_DOCUMENT_ROOT/index.html
 
-echo "[1] dumpio log"
-curl -s http://localhost:$AREX_PORT/
-grep 'mod_dumpio:  dumpio_in (data-HEAP): Host: localhost' $AREX_RUN_DIR/error_log || exit_code=1
-grep 'mod_dumpio:  dumpio_out (data-MMAP): index'          $AREX_RUN_DIR/error_log || exit_code=1
-
+echo "[1] dumpio log of simple GET"
+curl -H 'Range: bytes=4-8' -s "http://localhost:$AREX_PORT/?genus-name=ips&species-name=duplicatus"
+echo REQUEST
+grep 'dumpio_in (data'  $AREX_RUN_DIR/error_log
+echo RESPONSE
+grep 'dumpio_out (data' $AREX_RUN_DIR/error_log
+echo
+grep 'mod_dumpio:  dumpio_in .*: Host: localhost' $AREX_RUN_DIR/error_log || exit_code=1
+grep 'mod_dumpio:  dumpio_out .*: ndex'           $AREX_RUN_DIR/error_log || exit_code=1
 
 exit $exit_code
