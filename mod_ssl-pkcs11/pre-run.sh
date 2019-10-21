1925657f21a16fd7953dc0c2c47bbc2fb313f483
pgajdos <pgajdos@suse.cz>
2019-10-21 17:29:54 +0200

    add mod_ssl-pkcs11

---
+++ mod_ssl-pkcs11/pre-run.sh
@@ -0,0 +1,47 @@
+#
+# create CA certificate and SERVER certificate
+#
+. ../lib/openssl
+. ../lib/softhsm
+
+echo Setup CA
+echo ~~~~~~~~
+openssl_setup_ca $AREX_RUN_DIR
+echo
+echo Setup SERVER 
+echo ~~~~~~~~~~~~
+openssl_setup_entity $AREX_RUN_DIR aserver.suse.cz
+echo
+
+#
+# create a softhsm token with server key on it
+#
+# create the token
+echo '--- Initializing softhsm2 ------------------'
+success='yes'
+softhsm2_create_token "$AREX_RUN_DIR/pkcs11" 'aserver.suse.cz-token' || success='no'
+if [ $success == 'yes' ]; then
+  echo '--- Done. ----------------------------------'
+else
+  echo '--- Failed. --------------------------------'
+fi
+# openssl_setup_entity writes $AREX_RUN_DIR/aserver.suse.cz/private.key in
+# PEM format, we need DER
+echo 'Converting PEM to DER'
+openssl_pem_to_der $AREX_RUN_DIR/aserver.suse.cz/private.key $AREX_RUN_DIR/aserver.suse.cz/private.key.der
+# load the key in DER format
+echo "--- Write key to token ---------------------"
+success='yes'
+softhsm2_token_load_file "$AREX_RUN_DIR/pkcs11" 'aserver.suse.cz-token' 010203 $AREX_RUN_DIR/aserver.suse.cz/private.key.der 'aserver.suse.cz-privkey' privkey || success='no'
+if [ $success == 'yes' ]; then
+  echo '--- Done. ----------------------------------'
+else
+  echo '--- Failed. --------------------------------'
+fi
+# create server environment, need SOFTHSM2_CONF exported
+echo "export SOFTHSM2_CONF=\"$AREX_RUN_DIR/pkcs11/softhsm.conf\"" > $AREX_RUN_DIR/server_environment
+#
+# create runtime dir, see DefaultRuntimeDir directive
+#
+mkdir -p $AREX_RUN_DIR/run
+exit 0
---
+++ mod_ssl-pkcs11/run.sh
@@ -0,0 +1,12 @@
+exit_code=0
+
+echo 'Test SSL' > $AREX_DOCUMENT_ROOT/index.html
+
+echo "[1] access trough https allowed"
+curl -s --cacert $AREX_RUN_DIR/ca/my.crt --resolve "aserver.suse.cz:$AREX_PORT:127.0.0.1" https://aserver.suse.cz:$AREX_PORT/ \
+    | grep 'Test SSL' || exit_code=1
+
+echo "[2] error_log contains references to pkcs11"
+grep 'pkcs11:token=aserver.suse.cz-token' $AREX_RUN_DIR/error_log || exit_code=2
+
+exit $exit_code
