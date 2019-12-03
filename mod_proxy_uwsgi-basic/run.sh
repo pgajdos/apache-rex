2e75d0d8ee0c6cef2d60faddd652bdaa89151d19
pgajdos <pgajdos@suse.cz>
2019-12-03 11:20:36 +0100

    add mod_proxy_uwsgi-basic

---
+++ mod_proxy_uwsgi-basic/run.sh
@@ -0,0 +1,29 @@
+exit_code=0
+
+# install an application
+cp helloworld.py $AREX_RUN_DIR/
+# run uwsgi daemon
+echo -n "Starting uWSGI daemon .. "
+uwsgi --plugin $AREX_UWSGI_PLUGIN_HTTP,$AREX_UWSGI_PLUGIN_PYTHON \
+      --http localhost:$AREX_UWSGI_PORT --wsgi-file $AREX_RUN_DIR/helloworld.py \
+      --pidfile $AREX_RUN_DIR/uwsgi.pid \
+      --logto $AREX_RUN_DIR/uwsgi.log &
+sleep 1
+if [ -e "$AREX_RUN_DIR/uwsgi.pid" ]; then
+  echo "$(cat $AREX_RUN_DIR/uwsgi.pid)."
+else
+  echo "FAILED, see $AREX_RUN_DIR/uwsgi.log."
+  exit 1
+fi
+echo
+
+echo "[1] run an uWSGI application"
+curl -s http://localhost:$AREX_UWSGI_PORT/ | grep "Hello World!" || exit_code=1
+
+# stop uwsgi daemon
+echo
+echo -n "Stopping uWSGI daemon .."
+kill $(cat $AREX_RUN_DIR/uwsgi.pid) && echo 'done.' || echo 'FAILED.'
+
+exit $exit_code
+
