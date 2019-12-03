3b5e83144b430bab4cac694632ad9293a2e4a8ac
pgajdos <pgajdos@suse.cz>
2019-12-03 13:34:25 +0100

    run-rex: allow second uwsgi server; mod_proxy_uwsgi: add http subexample

--- mod_proxy_uwsgi-basic/run.sh
+++ mod_proxy_uwsgi-basic/run.sh
@@ -2,29 +2,46 @@ exit_code=0
 
 # install an application
 cp helloworld.py $AREX_RUN_DIR/
-# run uwsgi daemon
-echo -n "Starting uWSGI daemon .. "
+# start uwsgi daemons
+echo -n "Starting first uWSGI daemon (speaks uwsgi) .. "
 uwsgi --plugin $AREX_UWSGI_PLUGIN_PYTHON \
       --socket localhost:$AREX_UWSGI_PORT \
       --wsgi-file $AREX_RUN_DIR/helloworld.py \
-      --pidfile $AREX_RUN_DIR/uwsgi.pid \
-      --logto $AREX_RUN_DIR/uwsgi.log &
+      --pidfile $AREX_RUN_DIR/uwsgi-1.pid \
+      --logto $AREX_RUN_DIR/uwsgi-1.log &
 sleep 1
-if [ -e "$AREX_RUN_DIR/uwsgi.pid" ]; then
-  echo "$(cat $AREX_RUN_DIR/uwsgi.pid)."
+if [ -e "$AREX_RUN_DIR/uwsgi-1.pid" ]; then
+  echo "$(cat $AREX_RUN_DIR/uwsgi-1.pid)."
 else
-  echo "FAILED, see $AREX_RUN_DIR/uwsgi.log."
+  echo "FAILED, see $AREX_RUN_DIR/uwsgi-1.log."
+  exit 1
+fi
+echo -n "Starting second uWSGI daemon (speaks http) .. "
+uwsgi --plugin $AREX_UWSGI_PLUGIN_PYTHON,$AREX_UWSGI_PLUGIN_HTTP \
+      --http localhost:$AREX_UWSGI_PORT2 \
+      --wsgi-file $AREX_RUN_DIR/helloworld.py \
+      --pidfile $AREX_RUN_DIR/uwsgi-2.pid \
+      --logto $AREX_RUN_DIR/uwsgi-2.log &
+sleep 1
+if [ -e "$AREX_RUN_DIR/uwsgi-2.pid" ]; then
+  echo "$(cat $AREX_RUN_DIR/uwsgi-2.pid)."
+else
+  echo "FAILED, see $AREX_RUN_DIR/uwsgi-2.log."
   exit 1
 fi
 echo
 
-echo "[1] run an uWSGI application"
-curl -s http://localhost:$AREX_PORT/ | grep "Hello World!" || exit_code=1
+echo "[1] run an uWSGI application, uwsgi protocol"
+curl -s http://localhost:$AREX_PORT/uwsgi/ #| grep "Hello World!" || exit_code=1
+
+echo "[2] run an uWSGI application, http protocol"
+curl -s http://localhost:$AREX_PORT/http/ | grep "Hello World!" || exit_code=2
 
 # stop uwsgi daemon
 echo
-echo -n "Stopping uWSGI daemon .."
-kill $(cat $AREX_RUN_DIR/uwsgi.pid) && echo 'done.' || echo 'FAILED.'
+echo -n "Stopping uWSGI daemons .. "
+kill $(cat $AREX_RUN_DIR/uwsgi-1.pid) $(cat $AREX_RUN_DIR/uwsgi-2.pid) \
+  && echo -n 'done.' || echo 'FAILED.'
 
 exit $exit_code
 
