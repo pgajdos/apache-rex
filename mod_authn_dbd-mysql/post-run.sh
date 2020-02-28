41e5556dae904f69051288267a2a28a228bfd8e9
pgajdos <pgajdos@suse.cz>
2020-02-28 16:07:36 +0100

    refactor mod_authn_dbd-mysql

--- mod_authn_dbd-mysql/post-run.sh
+++ mod_authn_dbd-mysql/post-run.sh
@@ -1,10 +1,5 @@
+. ../lib/mysql
 mysql_dir=$AREX_RUN_DIR/mysql
 
-echo
-echo '>>> Shutting the mysql server down'
-cat << EOF > $mysql_dir/shutdown.sql
-SHUTDOWN;
-EOF
-mysql            --defaults-file=$mysql_dir/my.cnf --user root --password='roots_secret' < $mysql_dir/shutdown.sql
-sleep 2
+mysql_shutdown_server $mysql_dir
 echo
--- mod_authn_dbd-mysql/pre-run.sh
+++ mod_authn_dbd-mysql/pre-run.sh
@@ -1,3 +1,5 @@
+. ../lib/mysql
+
 # create password database
 mysql_dir=$AREX_RUN_DIR/mysql
 mkdir -p $mysql_dir
@@ -5,33 +7,10 @@ mkdir -p $mysql_dir
 john_sha1_passwd=$(htpasswd -nbs john johns_secret | head -n 1 | sed 's/john://')
 hans_sha1_passwd=$(htpasswd -nbs hans hans_secret | head -n 1 | sed 's/hans://')
 
-
-mkdir $mysql_dir/datadir $mysql_dir/datadir-private
-cat << EOF > $mysql_dir/my.cnf
-[client]
-user   = $AREX_USER
-socket = $mysql_dir/mysql.sock
-
-[mysqld]
-user=$AREX_USER
-log-error        = $mysql_dir/mysqld.log
-secure_file_priv = $mysql_dir/datadir-private
-datadir          = $mysql_dir/datadir
-server-id        = 1
-socket           = $mysql_dir/mysql.sock
-EOF
-
-echo '>>> Initializing databases'
-# --force is required by at least 10.0.38 on SLE 12 change root
-mysql_install_db --defaults-file=$mysql_dir/my.cnf --force
-
-echo '>>> Invoking mysqld'
-mysqld           --defaults-file=$mysql_dir/my.cnf&
-sleep 2
-
-echo '>>> Creating authentication database'
-mysqladmin       --defaults-file=$mysql_dir/my.cnf --user root password 'roots_secret'
-mysqladmin       --defaults-file=$mysql_dir/my.cnf --user root --password='roots_secret' create httpd_auth
+mysql_create_config $mysql_dir
+mysql_initialize_db $mysql_dir
+mysql_invoke_mysqld $mysql_dir
+mysql_create_database $mysql_dir 'httpd_auth'
 
 echo '>>> Creating authentication table'
 cat << EOF > $mysql_dir/auth-db-setup.sql
@@ -48,7 +27,7 @@ groups varchar(255),
 primary key (username)
 );
 EOF
-mysql            --defaults-file=$mysql_dir/my.cnf --user root --password='roots_secret' < $mysql_dir/auth-db-setup.sql
+mysql_invoke_script $mysql_dir $mysql_dir/auth-db-setup.sql
 
 echo '>>> Filling authentication table with example data'
 cat << EOF > $mysql_dir/insert-users.sql
@@ -56,15 +35,16 @@ USE httpd_auth;
 INSERT INTO password_table (username, passwd, groups) VALUES('john', '$john_sha1_passwd', 'employees');
 INSERT INTO password_table (username, passwd, groups) VALUES('hans', '$hans_sha1_passwd', 'customers');
 EOF
-mysql  --defaults-file=$mysql_dir/my.cnf --user auth_admin --password='auth_admin_secret' < $mysql_dir/insert-users.sql
+mysql_invoke_script $mysql_dir $mysql_dir/insert-users.sql auth_admin 'auth_admin_secret'
 
 echo '>>> Testing the mysql is running and database is ready via password query'
 cat << EOF > $mysql_dir/password-get.sql
 USE httpd_auth;
 SELECT passwd FROM password_table WHERE username = 'hans';
 EOF
-mysql  --defaults-file=$mysql_dir/my.cnf --user auth_admin --password='auth_admin_secret' \
-                               < $mysql_dir/password-get.sql > $mysql_dir/test-password.txt
+mysql_invoke_script $mysql_dir $mysql_dir/password-get.sql auth_admin 'auth_admin_secret' \
+                               > $mysql_dir/test-password.txt
 grep "$hans_sha1_passwd" $mysql_dir/test-password.txt && echo "SUCCESS" || echo "FAILURE";
 
 echo
+
