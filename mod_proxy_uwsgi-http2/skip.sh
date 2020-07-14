f8023ffcd57bf27e1ff0277f1a6e926d63389d53
pgajdos <pgajdos@suse.cz>
2020-07-14 13:41:40 +0200

    mod_http2 will not work with prefork MPM

--- mod_proxy_uwsgi-http2/skip.sh
+++ mod_proxy_uwsgi-http2/skip.sh
@@ -1,2 +1,2 @@
-# REASON: uwsgi does not have http or a python plugin
-exit $([ -z "$AREX_UWSGI_PLUGIN_HTTP" -o -z "$AREX_UWSGI_PLUGIN_PYTHON" ])
+# REASON: uwsgi does not have http or a python plugin, prefork not supported by mod_http2
+exit $([ -z "$AREX_UWSGI_PLUGIN_HTTP" -o -z "$AREX_UWSGI_PLUGIN_PYTHON" ] || [ $AREX_MPM == "prefork" ])
