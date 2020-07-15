6be1972adb3ba5c777d2154ff05205e2cf22cd3f
pgajdos <pgajdos@suse.cz>
2020-07-15 10:28:11 +0200

    add uwsgi daemon lib

--- mod_proxy_uwsgi-basic/run.sh
+++ mod_proxy_uwsgi-basic/run.sh
@@ -1,47 +1,26 @@
 exit_code=0
 
+. ../lib/uwsgi
+
 # install an application
 cp helloworld.py $AREX_RUN_DIR/
 # start uwsgi daemons
-echo -n "Starting first uWSGI daemon (speaks uwsgi) .. "
-uwsgi --plugin $AREX_UWSGI_PLUGIN_PYTHON \
-      --socket localhost:$AREX_UWSGI_PORT \
-      --wsgi-file $AREX_RUN_DIR/helloworld.py \
-      --pidfile $AREX_RUN_DIR/uwsgi-1.pid \
-      --logto $AREX_RUN_DIR/uwsgi-1.log &
-sleep 1
-if [ -e "$AREX_RUN_DIR/uwsgi-1.pid" ]; then
-  echo "$(cat $AREX_RUN_DIR/uwsgi-1.pid)."
-else
-  echo "FAILED, see $AREX_RUN_DIR/uwsgi-1.log."
-  exit 1
-fi
-echo -n "Starting second uWSGI daemon (speaks http) .. "
-uwsgi --plugin $AREX_UWSGI_PLUGIN_PYTHON,$AREX_UWSGI_PLUGIN_HTTP \
-      --http localhost:$AREX_UWSGI_PORT2 \
-      --wsgi-file $AREX_RUN_DIR/helloworld.py \
-      --pidfile $AREX_RUN_DIR/uwsgi-2.pid \
-      --logto $AREX_RUN_DIR/uwsgi-2.log &
-sleep 1
-if [ -e "$AREX_RUN_DIR/uwsgi-2.pid" ]; then
-  echo "$(cat $AREX_RUN_DIR/uwsgi-2.pid)."
-else
-  echo "FAILED, see $AREX_RUN_DIR/uwsgi-2.log."
-  exit 1
-fi
+uwsgi_daemon_start '1-uwsgi' $AREX_UWSGI_PLUGIN_PYTHON \
+                   $AREX_RUN_DIR/helloworld.py $AREX_UWSGI_PORT
+uwsgi_daemon_start '2-http'  $AREX_UWSGI_PLUGIN_PYTHON,$AREX_UWSGI_PLUGIN_HTTP \
+                   $AREX_RUN_DIR/helloworld.py $AREX_UWSGI_PORT2
 echo
 
 echo "[1] run an uWSGI application, uwsgi protocol"
 curl -s http://localhost:$AREX_PORT/uwsgi/ | grep "Hello World!" || exit_code=1
 
 echo "[2] run an uWSGI application, http protocol"
-curl -s http://localhost:$AREX_PORT/http/ | grep "Hello World!" || exit_code=2
+curl -s http://localhost:$AREX_PORT/http/  | grep "Hello World!" || exit_code=2
 
-# stop uwsgi daemon
+# stop uwsgi daemons
 echo
-echo -n "Stopping uWSGI daemons .. "
-kill $(cat $AREX_RUN_DIR/uwsgi-1.pid) $(cat $AREX_RUN_DIR/uwsgi-2.pid) \
-  && echo -n 'done.' || echo 'FAILED.'
+uwsgi_daemon_stop '1-uwsgi' $AREX_UWSGI_PORT
+uwsgi_daemon_stop '2-http'  $AREX_UWSGI_PORT2
 
 exit $exit_code
 
--- mod_proxy_uwsgi-http2/run.sh
+++ mod_proxy_uwsgi-http2/run.sh
@@ -1,34 +1,14 @@
 exit_code=0
 
+. ../lib/uwsgi
+
 # install an application
 cp helloworld.py $AREX_RUN_DIR/
 # start uwsgi daemons
-echo -n "Starting first uWSGI daemon (speaks uwsgi) .. "
-uwsgi --plugin $AREX_UWSGI_PLUGIN_PYTHON \
-      --socket localhost:$AREX_UWSGI_PORT \
-      --wsgi-file $AREX_RUN_DIR/helloworld.py \
-      --pidfile $AREX_RUN_DIR/uwsgi-1.pid \
-      --logto $AREX_RUN_DIR/uwsgi-1.log &
-sleep 1
-if [ -e "$AREX_RUN_DIR/uwsgi-1.pid" ]; then
-  echo "$(cat $AREX_RUN_DIR/uwsgi-1.pid)."
-else
-  echo "FAILED, see $AREX_RUN_DIR/uwsgi-1.log."
-  exit 1
-fi
-echo -n "Starting second uWSGI daemon (speaks http) .. "
-uwsgi --plugin $AREX_UWSGI_PLUGIN_PYTHON,$AREX_UWSGI_PLUGIN_HTTP \
-      --http localhost:$AREX_UWSGI_PORT2 \
-      --wsgi-file $AREX_RUN_DIR/helloworld.py \
-      --pidfile $AREX_RUN_DIR/uwsgi-2.pid \
-      --logto $AREX_RUN_DIR/uwsgi-2.log &
-sleep 1
-if [ -e "$AREX_RUN_DIR/uwsgi-2.pid" ]; then
-  echo "$(cat $AREX_RUN_DIR/uwsgi-2.pid)."
-else
-  echo "FAILED, see $AREX_RUN_DIR/uwsgi-2.log."
-  exit 1
-fi
+uwsgi_daemon_start '1-uwsgi' $AREX_UWSGI_PLUGIN_PYTHON \
+                   $AREX_RUN_DIR/helloworld.py $AREX_UWSGI_PORT
+uwsgi_daemon_start '2-http'  $AREX_UWSGI_PLUGIN_PYTHON,$AREX_UWSGI_PLUGIN_HTTP \
+                   $AREX_RUN_DIR/helloworld.py $AREX_UWSGI_PORT2
 echo
 
 echo "[1] run an uWSGI application, uwsgi protocol"
@@ -37,11 +17,10 @@ curl -s -k https://localhost:$AREX_PORT1/uwsgi/ | grep "Hello World!" || exit_co
 echo "[2] run an uWSGI application, http protocol"
 curl -s -k https://localhost:$AREX_PORT1/http/ | grep "Hello World!" || exit_code=2
 
-# stop uwsgi daemon
+# stop uwsgi daemons
 echo
-echo -n "Stopping uWSGI daemons .. "
-kill $(cat $AREX_RUN_DIR/uwsgi-1.pid) $(cat $AREX_RUN_DIR/uwsgi-2.pid) \
-  && echo -n 'done.' || echo 'FAILED.'
+uwsgi_daemon_stop '1-uwsgi' $AREX_UWSGI_PORT
+uwsgi_daemon_stop '2-http'  $AREX_UWSGI_PORT2
 
 exit $exit_code
 
